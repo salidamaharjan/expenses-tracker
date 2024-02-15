9149cccbee1b34ac5000762faffca63b105b66d1
CountDuckoo <countsuperc@gmail.com>
2024-02-14 19:40:03 -0600

    added initial route handling

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,33 @@
 const router = require('express').Router();
+const { Person } = require('../../models');
 
+// the 'api/users' endpoint
+
+// create a new user
+router.post('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+// logout
+router.post('/logout', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+const { Categories, Transactions, Person } = require('../models');
 
+// the '' endpoint
+
+// homepage
+router.get('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
 
 module.exports = router;
