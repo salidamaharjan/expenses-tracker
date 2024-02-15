95fa37d233fb070fbededbb938883d1ec25d5588
CountDuckoo <countsuperc@gmail.com>
2024-02-14 18:57:57 -0600

    added connection and basic routing files

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const transactionRoutes = require('./transactionRoutes');
+
+router.use('/users', userRoutes);
+router.use('/transactions', transactionRoutes);
+
+module.exports = router;
