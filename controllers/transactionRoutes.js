efe75b221ebbff0893d247a0ffe4dc7acaba2ab4
salidamaharjan <salidamaharjan@outlook.com>
2024-02-15 21:54:52 -0600

    view for addTrasaction

--- controllers/transactionRoutes.js
+++ controllers/transactionRoutes.js
@@ -2,26 +2,13 @@ const router = require('express').Router();
 const { Transactions, Categories, Person } = require('../models');
 const isAuthorized  = require('../utils/authorization');
 
-router.get('/', isAuthorized, async (req, res) => {
+router.get('/add', async (req, res) => {
   try{
-    const allTransactions = await Transactions.findAll({
-      include: {
-        model: Person,
-        attributes: ['id', 'username'],
-      },
-      include: {
-        model: Categories,
-      },
-    });
-    const transactions = allTransactions.map((transaction) =>
-      transaction.get({ plain: true })
-    );
-    res.render('transaction', {
-      transactions: transactions,
+    
+    res.render('addTransaction', {
       username: req.session.username,
       loggedIn: req.session.loggedIn,
     });
-    res.status(200).json(transactions);
   }catch(err){
     console.log(err);
     res.status(500).json(err);
