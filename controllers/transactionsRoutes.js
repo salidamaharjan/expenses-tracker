29d9d98ff4430c1898acf199d5ffe50af3389341
salidamaharjan <salidamaharjan@outlook.com>
2024-02-18 11:33:45 -0600

    send all categories to show while adding a transaction

--- controllers/transactionsRoutes.js
+++ controllers/transactionsRoutes.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 const { Transactions, Categories, Person } = require('../models');
-const isAuthorized  = require('../utils/authorization');
+const isAuthorized = require('../utils/authorization');
 
 router.get('/add', async (req, res) => {
-  try{
-    
+  try {
+    const allCategories = await Categories.findAll();
+    const categories = allCategories.map((category) => category.get({plain: true}));
     res.render('addTransaction', {
       username: req.session.username,
       loggedIn: req.session.loggedIn,
+      categories: categories,
     });
-  }catch(err){
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
